98d1922d4b087516740ceddfc4463e5ee02d0fb4
Charlton Moren <charlton.moren@gmail.com>
2026-03-13 03:06:38 -0500

    feat: update blocks

--- blocks.def.h
+++ blocks.def.h
@@ -3,9 +3,10 @@ static const Block blocks[] = {
 
 	{"VOL ",  "~/.local/bin/dwmblocks/volume",           2,          1},
 	{"BAT ",  "~/.local/bin/dwmblocks/battery",          30,         2},
+    //{"BRI ", "~/.local/bin/dwmblocks/brightness-status", 5,          0},
 	{"MEM ",  "~/.local/bin/dwmblocks/memory",           5,          0},
-	{"DISK ", "~/.local/bin/dwmblocks/disk",             60,         0},
-	//{"NET ",  "~/.local/bin/dwmblocks/network",          5,          0},
+	//{"DISK ", "~/.local/bin/dwmblocks/disk",             60,         0},
+	{"NET ",  "~/.local/bin/dwmblocks/network",          5,          0},
 	{"",      "~/.local/bin/dwmblocks/datetime",         5,          0},
 };
 
